983a997f36a1f96915df82ae8f87152df9cbc33f
codingpixels-FF <codingpixels@proton.me>
2026-03-07 22:09:14 +0100

    fix: color lines on texture, simple mouse support with left button

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ import (
 )
 
 const (
-	screenWidth      = 1920
-	screenHeight     = 1080
+	screenWidth      = 1920 / 4
+	screenHeight     = 1080 / 4
 	screenPixelSize  = 4
 	screenHeightHalf = screenHeight / 2
 	statusBarHeight  = screenHeight / 2
@@ -22,6 +22,12 @@ const (
 	renderHeightHalf = renderHeight / 2
 )
 
+type PixelColor struct {
+	R int
+	G int
+	B int
+}
+
 func main() {
 	// Set the maximum number of CPU cores to use
 	// Set to debug or trace level for detailed logs
@@ -29,7 +35,7 @@ func main() {
 	raylib.InitWindow(screenWidth, screenHeight+statusBarHeight, "Image mapping on objects in Go")
 	defer raylib.CloseWindow()
 
-	wallTexture := raylib.LoadTexture("wall_all_small.png") // Loaded in GPU memory (VRAM)
+	wallTexture := raylib.LoadTexture("repeat_triangle_64.png") // Loaded in GPU memory (VRAM)
 	defer raylib.UnloadTexture(wallTexture)
 	wallImage := raylib.LoadImageFromTexture(wallTexture)      // Loaded in CPU memory (RAM)
 	raylib.ImageFormat(wallImage, raylib.UncompressedR8g8b8a8) // Format image to RGBA 32bit (required for texture update)
@@ -81,6 +87,19 @@ func main() {
 	}
 
 	angle := 0.0
+	offsetX := 20
+	offsetY := 20
+	lastMouseAbsX := -1
+	lastMouseAbsY := -1
+	colorR := 0
+	colorG := 0
+	colorB := 0
+	pixelColor := PixelColor{
+		R: colorR,
+		G: colorG,
+		B: colorB,
+	}
+
 	for !raylib.WindowShouldClose() {
 		var wg sync.WaitGroup
 		// Check if we point to pixelBuffer2
@@ -105,14 +124,38 @@ func main() {
 				}*/
 			}()
 		}
-		offsetX := 20
-		offsetY := 20
+
 		prefillBUfferWithImage(offsetX, offsetY, wallImage, wallImageImage, currentPixelBuffer)
 		// Mouse
 		mouseAbsX := int(raylib.GetMouseX())
 		mouseAbsY := int(raylib.GetMouseY())
+		mouseButton1Pressed := raylib.IsMouseButtonDown(raylib.MouseButtonLeft)
+		if mouseButton1Pressed {
+			if lastMouseAbsX != -1 || lastMouseAbsY != -1 {
+				setLineColor(wallImage, lastMouseAbsX-offsetX, lastMouseAbsY-offsetY, mouseAbsX-offsetX, mouseAbsY-offsetY, pixelColor)
+			}
 
-		setPixelWhite(wallImage, mouseAbsX-offsetX, mouseAbsY-offsetY)
+			//setPixelWhite(wallImage, mouseAbsX-offsetX, mouseAbsY-offsetY)
+			lastMouseAbsX = mouseAbsX
+			lastMouseAbsY = mouseAbsY
+			if mouseAbsX > 100 && mouseAbsX < 355 {
+				if mouseAbsY > 0 && mouseAbsY < 10 {
+					colorR = mouseAbsX - 100
+				}
+				if mouseAbsY > 10 && mouseAbsY < 20 {
+					colorG = mouseAbsX - 100
+				}
+				if mouseAbsY > 20 && mouseAbsY < 30 {
+					colorB = mouseAbsX - 100
+				}
+				pixelColor = PixelColor{
+					R: colorR,
+					G: colorG,
+					B: colorB,
+				}
+			}
+		}
+
 		wallImageImage = wallImage.ToImage()
 
 		// Switch the image buffer
@@ -133,50 +176,57 @@ func main() {
 		raylib.DrawTexturePro(imageBufferTexture, textureRectangle, finalRenderRectagle, raylib.Vector2{}, 0, raylib.White)
 
 		// status bar overlay
-		raylib.DrawRectangle(0, screenHeight, screenWidth, screenHeight+10, raylib.NewColor(0, 0, 128, 255))
-		raylib.DrawRectangle(0, screenHeight+10, screenWidth, screenHeight+statusBarHeight, raylib.NewColor(0, 0, 255, 255))
+		//raylib.DrawRectangle(0, screenHeight, screenWidth, screenHeight+10, raylib.NewColor(0, 0, 128, 255))
+		//raylib.DrawRectangle(0, screenHeight+10, screenWidth, screenHeight+statusBarHeight, raylib.NewColor(0, 0, 255, 255))
 		//playerStatus := fmt.Sprintf("%.2f\n%.2f\n%.2f", player.x, player.y, player.angle)
-		playerStatus := fmt.Sprintf("%.2d\n%.2d", mouseAbsX, mouseAbsY)
-		raylib.DrawText(playerStatus, 20, screenHeight+10, 65, raylib.White)
+		playerStatus := fmt.Sprintf("%.2d\n%.2d\n%.2d\n%.2d\n%.2d", mouseAbsX, mouseAbsY, colorR, colorG, colorB)
+		raylib.DrawText(playerStatus, 20, screenHeight+10, 15, raylib.White)
 
-		playerStatusLegend := fmt.Sprintf("x\ny\n")
-		raylib.DrawText(playerStatusLegend, 200, screenHeight+10, 65, raylib.White)
-		raylib.DrawFPS(screenWidth-90, screenHeight+10)
+		playerStatusLegend := fmt.Sprintf("x\ny\nR\nG\nB\n")
+		raylib.DrawText(playerStatusLegend, 50, screenHeight+10, 15, raylib.White)
 
 		raylib.BeginMode3D(camera)
 
 		// Draw cube with an applied texture
-		vec := raylib.Vector3{
-			X: -2.0,
-			Y: 2.0,
-		}
 
 		currentTexture := raylib.LoadTextureFromImage(wallImage)
 
-		DrawCubeTexture(currentTexture, vec, 2.0, 2.0, 2.0, raylib.White)
-		vec = raylib.Vector3{
-			X: 0.0,
-			Y: 1.0,
-		}
-		DrawCubeTexture(currentTexture, vec, 2.0, 2.0, 2.0, raylib.White)
+		for xx := float32(-1.0); xx <= 1.0; xx = xx + 1.0 {
 
-		vec = raylib.Vector3{
-			X: 2.0,
-			Y: 1.0,
+			vec := raylib.Vector3{
+				X: xx,
+				Y: -1.0,
+			}
+			DrawCubeTexture(currentTexture, vec, 1.0, 1.0, 1.0, raylib.White)
+
+			vec = raylib.Vector3{
+				X: xx,
+				Y: 1.0,
+			}
+			DrawCubeTexture(currentTexture, vec, 1.0, 1.0, 1.0, raylib.White)
+
+			if xx != 0 {
+				vec = raylib.Vector3{
+					X: xx,
+					Y: 0.0,
+				}
+				DrawCubeTexture(currentTexture, vec, 1.0, 1.0, 1.0, raylib.White)
+			}
 		}
+
 		// Increase the angle (adjust speed as needed)
 		angle += 0.05
 
 		// Calculate new camera position to orbit around the target (center)
-		radius := float32(10.0)
+		radius := float32(4.0)
 		camera.Position.X = float32(math.Cos(float64(angle))) * radius
 		camera.Position.Z = float32(math.Sin(float64(angle))) * radius
-		camera.Position.Y = 10.0 // Keep height constant, or change for a different effect
+		camera.Position.Y = 4.0 // Keep height constant, or change for a different effect
 
 		// Make sure to update the target if needed, or keep it fixed
 		camera.Target = raylib.Vector3{0, 0, 0}
 
-		raylib.DrawGrid(10, 1.0) // Draw a grid
+		//raylib.DrawGrid(10, 1.0) // Draw a grid
 		raylib.EndMode3D()
 		raylib.DrawFPS(screenWidth-100, 10)
 		raylib.EndDrawing()
@@ -184,29 +234,134 @@ func main() {
 	}
 }
 
-func setPixelWhite(img *raylib.Image, x, y int) {
+func absDiffInt(x, y int) int {
+	if x < y {
+		return y - x
+	}
+	return x - y
+}
+
+func setLineColor(img *raylib.Image, lastX, lastY, currentX, currentY int, pixelColor PixelColor) {
+	/*if lastX < 0 || lastX >= int(img.Width) || lastY < 0 || lastY >= int(img.Height) {
+		return // out of bounds
+	}
+	if currentX < 0 || currentX >= int(img.Width) || currentY < 0 || currentY >= int(img.Height) {
+		return // out of bounds
+	}*/
+	//setPixelWhite(img, currentX, currentY)
+	//return
+	// Bresenham's line
+	dx := absDiffInt(currentX, lastX)
+	dy := absDiffInt(currentY, lastY)
+
+	var sx, sy int
+	if lastX < currentX {
+		sx = 1
+	} else {
+		sx = -1
+	}
+
+	if lastY < currentY {
+		sy = 1
+	} else {
+		sy = -1
+	}
+
+	balanceErr := dx - dy
+
+	x, y := lastX, lastY
+
+	for {
+		setPixelWhite(img, x, y, pixelColor)
+		if x == currentX && y == currentY {
+			break
+		}
+		e2 := 2 * balanceErr
+		if e2 > -dy {
+			balanceErr -= dy
+			x += sx
+		}
+		if e2 < dx {
+			balanceErr += dx
+			y += sy
+		}
+	}
+}
+
+func setPixelWhite(img *raylib.Image, x, y int, pixelColor PixelColor) {
 	if x < 0 || x >= int(img.Width) || y < 0 || y >= int(img.Height) {
 		return // out of bounds
 	}
 
-	bytesPerPixel := 4 // RGBA
-	index := (y*int(img.Width) + x) * bytesPerPixel
+	/*bytesPerPixel := 4 // RGBA
+
+	reverseX := (int(img.Width) - 1 - x)
+	reverseY := (int(img.Height) - 1 - y)
+
 	size := int(img.Width) * int(img.Height) * bytesPerPixel
 
 	// Convert unsafe.Pointer to a byte slice with the correct length
 	dataSlice := (*[1 << 30]byte)(img.Data)[:size:size]
 
 	// Set pixel to white
-	dataSlice[index] = 255   // R
-	dataSlice[index+1] = 255 // G
-	dataSlice[index+2] = 255 // B
-	dataSlice[index+3] = 255 // A
+	indexLeftTop := (y*int(img.Width) + x) * bytesPerPixel
+	dataSlice[indexLeftTop] = 255   // R
+	dataSlice[indexLeftTop+1] = 255 // G
+	dataSlice[indexLeftTop+2] = 255 // B
+	dataSlice[indexLeftTop+3] = 255 // A
+
+	// Set pixel to white
+	indexRightBottom := (reverseY*int(img.Width) + reverseX) * bytesPerPixel
+	dataSlice[indexRightBottom] = 255   // R
+	dataSlice[indexRightBottom+1] = 255 // G
+	dataSlice[indexRightBottom+2] = 255 // B
+	dataSlice[indexRightBottom+3] = 255 // A
+
+	// Set pixel to white
+	indexRightTop := (y*int(img.Width) + reverseX) * bytesPerPixel
+	dataSlice[indexRightTop] = 255   // R
+	dataSlice[indexRightTop+1] = 255 // G
+	dataSlice[indexRightTop+2] = 255 // B
+	dataSlice[indexRightTop+3] = 255 // A
+
+	// Set pixel to white
+	indexLeftBottom := (reverseY*int(img.Width) + x) * bytesPerPixel
+	dataSlice[indexLeftBottom] = 255   // R
+	dataSlice[indexLeftBottom+1] = 255 // G
+	dataSlice[indexLeftBottom+2] = 255 // B
+	dataSlice[indexLeftBottom+3] = 255 // A*/
+
+	// TODO FLIPP X FOR Y and LX FOR LY
+
+	width, height := int(img.Width), int(img.Height)
+	bytesPerPixel := 4
+
+	dataSlice := (*[1 << 30]byte)(img.Data)[: width*height*bytesPerPixel : width*height*bytesPerPixel]
+
+	// Generate symmetric positions
+
+	xs := []int{x, width - 1 - x, y, height - 1 - y}
+
+	ys := []int{y, height - 1 - y, x, width - 1 - x}
+
+	for _, xi := range xs {
+		for _, yi := range ys {
+			if xi >= 0 && xi < width && yi >= 0 && yi < height {
+				index := (yi*width + xi) * bytesPerPixel
+				dataSlice[index] = byte(pixelColor.R)   //R
+				dataSlice[index+1] = byte(pixelColor.G) //G
+				dataSlice[index+2] = byte(pixelColor.B) //B
+				dataSlice[index+3] = 255                //A
+			}
+		}
+	}
+
 }
 
 func prefillBUfferWithImage(offsetX int, offsetY int, wallImage *raylib.Image, wallImageImage image.Image, currentPixelBuffer *[]uint32) {
-	for rayX := offsetX; rayX < int(wallImage.Width); rayX++ {
+	for rayX := offsetX; rayX < int(wallImage.Width)+offsetX; rayX++ {
 		corX := int(int32(rayX)) - offsetX
-		for rayY := offsetY; rayY < int(wallImage.Height); rayY++ {
+		for rayY := offsetY; rayY < int(wallImage.Height)+offsetY; rayY++ {
 			// Extract fill color and alpha
 			fillColor := wallImageImage.At(corX, int(int32(rayY))-offsetY)
 			r, g, b, a := fillColor.RGBA()
